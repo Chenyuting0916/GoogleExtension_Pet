841d6f88531f1d23a572017afba09a133389237a
LU <monkey07233@gmail.com>
2020-03-31 12:00:15 +0800

    add pet

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-'use strict';
-
+document.addEventListener("DOMContentLoaded", function() {
+  var add = document.getElementById("add");
+  add.addEventListener("click", function() {
+    chrome.tabs.executeScript(null, {file: "pet.js"});
+  });
+});
